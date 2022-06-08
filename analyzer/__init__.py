27dbc8d6e98c19b18a2d5f4ee8520b971e358ba9
Frostshockk <72066925+Frostshockk@users.noreply.github.com>
2022-06-08 11:28:35 +0300

    Add files via upload

---
+++ analyzer/__init__.py
@@ -0,0 +1,18 @@
+"""
+Сервис с REST API, анализирующий рынок для промо-акций.
+"""
+__author__ = 'Gorokhov Kirill'
+__maintainer__ = __author__
+
+__email__ = 'studentGorokhov@gmail.com'
+__license__ = ' '
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
